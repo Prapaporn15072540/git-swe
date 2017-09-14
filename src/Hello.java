33b4bc7dfe590d4b05eebc56d5f4f48596823f18
อิเคย <@DESKTOP-4AI1B3D>
2017-09-14 08:20:22 +0700

    kuy

--- src/Hello.java
+++ src/Hello.java
@@ -6,6 +6,8 @@ public class Hello {
 		System.out.println("Hello World");
 		System.out.println("Hello Tudcook");
 		System.out.println("Hello tum");
+		System.out.println("Hello cookkuy");
+		
 	}
 
 }
