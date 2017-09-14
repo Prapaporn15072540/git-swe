3996d5bb2f8e7ce111f1e909e7ba65f0ae01c30f
DESKTOP-QQ4UMN4\asus <Pongsatorn698@gmail.com>
2017-09-14 09:39:01 +0700

    Calculater

---
+++ src/Result.java
@@ -0,0 +1,17 @@
+
+public class Result  {
+	public int n;
+	public Result(int n) {
+		this.n=n;
+		
+		// TODO Auto-generated method stub
+		if(n%2==0) {
+			System.out.println(n);
+		}
+}
+	public String output() {
+		return null;
+	}
+	
+	
+}
