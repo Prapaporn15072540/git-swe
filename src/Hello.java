3c8eaecf6fed619a0573a89977ced85495248a92
DESKTOP-QQ4UMN4\asus <Pongsatorn698@gmail.com>
2017-09-14 08:32:26 +0700

    kkkkk

--- src/Hello.java
+++ src/Hello.java
@@ -4,9 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Hello Tudcook");
-	
-		
+		System.out.println("Hello Tudcook");	
 	}
-
 }
