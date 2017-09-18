79b6af2e2a28201f9c33d6cb360bb3fef2e8d9f8
อิเคย <@DESKTOP-4AI1B3D>
2017-09-18 13:27:40 +0700

    Calculate V1

--- src/Hello.java
+++ src/Hello.java
@@ -4,8 +4,11 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a number: ");
-		int n = input.nextInt(); 
+		int n = input.nextInt();
+		Result cal = new Result(n);
+		System.out.println(cal.output());
 	}
 }
