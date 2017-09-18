0e7537ee7bc51e340ade183c1c5a75da937fabba
อิเคย <@DESKTOP-4AI1B3D>
2017-09-18 14:32:59 +0700

    1

--- src/hum/Student.java
+++ src/hum/Student.java
@@ -1,10 +1,15 @@
 package hum;
 
+import java.util.Scanner;
+
 public class Student {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = input.nextInt();
+		
 	}
 
 }
