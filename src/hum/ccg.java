ede655e34dead203568535188960560fbde4e4d4
อิเคย <@DESKTOP-4AI1B3D>
2017-09-19 11:36:21 +0700

    change calculate project A Minor

--- src/hum/ccg.java
+++ src/hum/ccg.java
@@ -3,13 +3,13 @@ package hum;
 public class ccg {
 	public double cal(double sale, double salary) {
 	double sum = 0;
-	if(sale>=1||sale<=25000)
+	if(sale>=1||sale<=50000)
 	{
 		sum = salary+(sale*0.01);
 	}
-	if(sale>=25001||sale<=50000){
+	if(sale>=50001||sale<=100000){
 		sum = salary+(sale*0.02);
-	}if(sale>=50001) {
+	}if(sale>=100001) {
 		sum = salary+(sale*0.03);
 		
 	}
