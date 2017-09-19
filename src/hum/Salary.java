9c3ba32b176ffe638fbd35469f9a12ef6a65fdf9
อิเคย <@DESKTOP-4AI1B3D>
2017-09-19 10:33:42 +0700

    main

--- src/hum/Salary.java
+++ src/hum/Salary.java
@@ -7,9 +7,12 @@ public class Salary {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter: ");
-		int n = input.nextInt();
-		
+		System.out.println("Enter a ID_Employee: ");
+		String id = input.nextLine();
+		System.out.println("Enter a Name: ");
+		String name = input.nextLine();
+		System.out.println("Enter a Salary: ");
+		int salary = input.nextInt();
 	}
 
 }
