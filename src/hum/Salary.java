df4bddf27ac8ce8b9dd0878fa3ed843e76e7b05b
Prapaporn15072540 <@DESKTOP-4AI1B3D>
2017-09-19 10:25:13 +0700

    f

---
+++ src/hum/Salary.java
@@ -0,0 +1,15 @@
+package hum;
+
+import java.util.Scanner;
+
+public class Salary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter: ");
+		int n = input.nextInt();
+		
+	}
+
+}
